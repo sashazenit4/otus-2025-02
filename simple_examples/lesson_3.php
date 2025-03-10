680518f0ab88e417b98c588b9b7af6a0be8740ac
Aleksandr Kholin <holinshura@gmail.com>
2025-03-10 21:20:42 +0300

    chore(var-dumper): add vur-dumper, add example

---
+++ simple_examples/lesson_3.php
@@ -0,0 +1,10 @@
+<?php
+require $_SERVER['DOCUMENT_ROOT'] . '/bitrix/header.php';
+
+dump([
+    'Жена' => '+77777777777',
+    'Мама' => '+78888888888' 
+]);
+
+
+require $_SERVER['DOCUMENT_ROOT'] . '/bitrix/footer.php';
