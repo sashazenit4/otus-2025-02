0991e5622ce2a30ea1ac1be9f2a8236c8e00d069
Aleksandr Kholin <holinshura@gmail.com>
2025-05-23 19:15:21 +0300

    feat(lecture): js events

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -8,4 +8,10 @@ if (file_exists(__DIR__ . '/src/autoloader.php')) {
 if (file_exists(__DIR__ . '/events.php')) {
     require_once __DIR__ . '/events.php';
 }
-\Bitrix\Main\UI\Extension::load(['aholin_crmcustomtab.useless_extensions.greeting-message']);
+\Bitrix\Main\UI\Extension::load([
+//    'aholin_crmcustomtab.useless_extensions.greeting-message',
+//    'dev_helper.log_events',
+//    'ajax.all_ajax_handler',
+//    'otus_crm.negative_currency',
+    'homework.begin_date_button',
+]);
