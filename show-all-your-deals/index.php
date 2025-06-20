e141fd7da4f7e52f0f8542f45fcfe652a87b1a55
Aleksandr Kholin <holinshura@gmail.com>
2025-06-20 21:24:57 +0300

    feat(lecture): small update in index.php in multi-portal application

--- show-all-your-deals/index.php
+++ show-all-your-deals/index.php
@@ -1,14 +1,12 @@
 <?php
 require_once (__DIR__.'/crest_ext.php');
 CRest_Ext::setCurrentBitrix24($_REQUEST['member_id']);
-$result = CRest_Ext::call('crm.deal.list');
-var_dump($result);
-
+$result = CRest_Ext::call('crm.lead.list');
 ?>
 <ul>
 <?php
-foreach ($result as $deal) { ?>
-    <li><?=$deal['TITLE']?></li>
+foreach ($result['result'] as $lead) { ?>
+    <li><?=$lead['TITLE']?></li>
 <?php
 }
 ?>
